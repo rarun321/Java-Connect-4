4f8d02b4e6da1b2c29a9da165075865feefa6970
rarun321 <43283635+rarun321@users.noreply.github.com>
2020-03-20 19:44:08 -0700

    Added GUI Application

---
+++ src/ui/Connect4GUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Connect4GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
